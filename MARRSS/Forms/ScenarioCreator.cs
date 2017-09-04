f70af6ed1e9fbec4ac2d92c024ed725a733eb30f
reed <nikolai.reed@gmx.de>
2017-09-04 19:47:59 +0200

    cleaned up Code,empty form for ScnearioBuilder

---
+++ MARRSS/Forms/ScenarioCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MARRSS.Forms
+{
+    public partial class ScenarioCreator : Form
+    {
+        public ScenarioCreator()
+        {
+            InitializeComponent();
+        }
+    }
+}
